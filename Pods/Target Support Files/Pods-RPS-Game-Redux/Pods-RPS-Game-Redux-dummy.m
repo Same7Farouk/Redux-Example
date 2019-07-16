fcd6476865e595fbf92a66f169fe510fd78e16a3
Sameh Farouk <sameh_mamdouh@hotmail.com>
2019-07-16 17:38:15 +0200

    ignore pod files

--- Pods/Target Support Files/Pods-RPS-Game-Redux/Pods-RPS-Game-Redux-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RPS_Game_Redux : NSObject
-@end
-@implementation PodsDummy_Pods_RPS_Game_Redux
-@end
--- Pods/Target Support Files/Pods-RPS-Game-ReduxTests/Pods-RPS-Game-ReduxTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RPS_Game_ReduxTests : NSObject
-@end
-@implementation PodsDummy_Pods_RPS_Game_ReduxTests
-@end
